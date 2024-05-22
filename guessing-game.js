4acf6176563390f5bac8274cf43e694af7b242be
Ben Briones <benbriones50@gmail.com>
2024-05-22 16:48:41 -0700

    finished guessing-game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,21 @@
 function guessingGame() {
+  const number = Math.floor(Math.random() * 100)
+  let guesses = 0;
+  let hasWon = false;
 
+  return function(guess) {
+    if (hasWon) return "The game is over, you already won!"
+    guesses++;
+
+    if (guess === number) {
+      hasWon = true;
+      return `You win! You found ${number} in ${guesses} guesses.`
+    } else  if ( guess < number) {
+      return `${guess} is too low!`
+    } else {
+      return `${guess} is too high!`
+    }
+  }
 }
 
 module.exports = { guessingGame };
